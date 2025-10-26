bb9ef0c79a8527eb4b1d1c770fd832b277509ed2
FelipeArce <arceprogramando@gmail.com>
2025-10-26 20:47:01 -0300

    test: Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.arceprog.dev',
+  site: 'https://arceprog.dev',
   compressHTML: true,
   integrations: [sitemap()],
   image: {
